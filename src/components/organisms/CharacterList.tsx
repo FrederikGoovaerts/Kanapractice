734f466fd2adb1925f63032b0a869071245105cd
Frederik Goovaerts <goovaertsfrederik@hotmail.com>
2018-11-26 23:00:52 +0100

    Better centering, but no spacing on non-5 card lines

--- src/components/organisms/CharacterList.tsx
+++ src/components/organisms/CharacterList.tsx
@@ -14,21 +14,23 @@ interface Props {
 
 export const CharacterList = (props: Props) => (
   <div className="characterList-container">
-    <Grid container direction="column" alignItems="center" spacing={16}>
-      {props.characters.map((group: CharacterGroup) => (
-        <Grid container item direction="row" justify="space-between" spacing={16} key={group.name}>
-          {group.characters.map((character: Character) => (
-            <Grid item key={character.id}>
-              <CharacterCard
-                key={character.roumaji}
-                character={character}
-                onSelect={props.onSelect}
-                selected={props.selected.includes(character.id)}
-              />
-            </Grid>
-          ))}
-        </Grid>
-      ))}
-    </Grid>
+    <div className="characterList-innerContainer">
+      <Grid container direction="column" spacing={16}>
+        {props.characters.map((group: CharacterGroup) => (
+          <Grid container item direction="row" justify="center" spacing={16} key={group.name}>
+            {group.characters.map((character: Character) => (
+              <Grid item key={character.id}>
+                <CharacterCard
+                  key={character.roumaji}
+                  character={character}
+                  onSelect={props.onSelect}
+                  selected={props.selected.includes(character.id)}
+                />
+              </Grid>
+            ))}
+          </Grid>
+        ))}
+      </Grid>
+    </div>
   </div>
 );
