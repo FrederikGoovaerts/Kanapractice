b1e9a5df5b988f8489fd97694434f38eedbfad10
Frederik Goovaerts <goovaertsfrederik@hotmail.com>
2018-11-27 22:44:03 +0100

    Looks good, but smaller screens have weird behaviour now.

--- src/components/organisms/CharacterList.tsx
+++ src/components/organisms/CharacterList.tsx
@@ -12,25 +12,54 @@ interface Props {
   selected: string[];
 }
 
-export const CharacterList = (props: Props) => (
-  <div className="characterList-container">
-    <div className="characterList-innerContainer">
-      <Grid container direction="column" spacing={16}>
-        {props.characters.map((group: CharacterGroup) => (
-          <Grid container item direction="row" justify="center" spacing={16} key={group.name}>
-            {group.characters.map((character: Character) => (
-              <Grid item key={character.id}>
-                <CharacterCard
-                  key={character.roumaji}
-                  character={character}
-                  onSelect={props.onSelect}
-                  selected={props.selected.includes(character.id)}
-                />
-              </Grid>
-            ))}
-          </Grid>
-        ))}
-      </Grid>
-    </div>
-  </div>
+const dummyCard = (key: string) => (
+  <Grid item key={key}>
+    <div className="characterCard-card" />
+  </Grid>
 );
+
+export class CharacterList extends React.Component<Props> {
+  renderCharacter(character: Character) {
+    return (
+      <Grid item key={character.id}>
+        <CharacterCard
+          key={character.roumaji}
+          character={character}
+          onSelect={this.props.onSelect}
+          selected={this.props.selected.includes(character.id)}
+        />
+      </Grid>
+    );
+  }
+
+  renderGroup(group: CharacterGroup) {
+    let dummyCounter = 0;
+    const cardList = group.characters.map((character: Character) => this.renderCharacter(character));
+
+    // When life gives you lemons ¯\_(ツ)_/¯
+    if (cardList.length < 5) {
+      cardList.splice(1, 0, dummyCard(String(dummyCounter++)));
+    }
+    while (cardList.length < 5) {
+      cardList.splice(-1, 0, dummyCard(String(dummyCounter++)));
+    }
+
+    return (
+      <Grid container item direction="row" justify="center" spacing={16} key={group.name}>
+        {cardList}
+      </Grid>
+    );
+  }
+
+  render() {
+    return (
+      <div className="characterList-container">
+        <div className="characterList-innerContainer">
+          <Grid container direction="column" spacing={16}>
+            {this.props.characters.map((group: CharacterGroup) => this.renderGroup(group))}
+          </Grid>
+        </div>
+      </div>
+    );
+  }
+}
